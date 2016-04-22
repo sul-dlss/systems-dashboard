c33a012bb2ab0906fac01eaf593b7fe140a74031
Jon Robertson <jonrober@stanford.edu>
2016-04-22 10:52:57 -0700

    Fixed flagging of column data on summary page
    Closes #1

--- spec/controllers/summary_controller_spec.rb
+++ spec/controllers/summary_controller_spec.rb
@@ -13,14 +13,17 @@ RSpec.describe SummaryController, type: :controller do
       it { is_expected.to render_template :index }
 
       it "gets the right output" do
+        # Two hosts.
         expect(response.body).to match(/<td>example<\/td>/)
-        expect(response.body).to match(/<a href="\/advisories\/example">1<\/a>/)
-        expect(response.body).to match(/<a href="\/ossec\/example">2<\/a>/)
-        expect(response.body).to match(/<td>1: Critical<\/td>/)
         expect(response.body).to match(/<td>test<\/td>/)
+
+        # For the first host, several fields that should show.
+        expect(response.body).to match(/<td class="flagged"><a href="\/ossec\/example">2<\/a><\/td>/)
+        expect(response.body).to match(/<td class="flagged"><a href="\/advisories\/example">1<\/a><\/td>/)
+        expect(response.body).to match(/<td>1: Critical<\/td>/)
       end
 
-      context 'displays a list of all servers' do
+      context 'displays only flagged servers' do
         before do
           stub_const('ApplicationController::YAML_DIR', 'spec/data/')
           get :index, show_only_flagged: 1
@@ -30,11 +33,14 @@ RSpec.describe SummaryController, type: :controller do
         it { is_expected.to render_template :index }
 
         it "gets the right output" do
+          # Only one host.
           expect(response.body).to match(/<td>example<\/td>/)
-          expect(response.body).to match(/<a href="\/advisories\/example">1<\/a>/)
-          expect(response.body).to match(/<a href="\/ossec\/example">2<\/a>/)
-          expect(response.body).to match(/<td>1: Critical<\/td>/)
           expect(response.body).not_to match(/<td>test<\/td>/)
+
+          # And the flagged data for it again.
+          expect(response.body).to match(/<td class="flagged"><a href="\/ossec\/example">2<\/a><\/td>/)
+          expect(response.body).to match(/<td class="flagged"><a href="\/advisories\/example">1<\/a><\/td>/)
+          expect(response.body).to match(/<td>1: Critical<\/td>/)
         end
       end
     end
