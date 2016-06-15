d177b3c2da53845594e627e96db7d0dca7fd4d28
Jon Robertson <jonrober@stanford.edu>
2016-06-15 14:32:07 -0700

    Add upgrades cache to scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,6 +11,11 @@ every '*/10 * * * *' do
   rake 'download:servers'
 end
 
+# Very low resource indeed.
+every '*/10 * * * *' do
+  rake 'download:upgrades'
+end
+
 # The ossec update process can take a while, so only every 30m.
 every '*/30 * * * *' do
   rake 'download:ossec'
