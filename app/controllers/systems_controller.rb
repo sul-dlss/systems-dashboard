1b823e34af7b92d2fd0f00ce592b64439b9927bc
Jon Robertson <jonrober@stanford.edu>
2016-06-24 16:14:00 -0700

    Move and expand data sources and their errors to new screen

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,6 +1,6 @@
 class SystemsController < ApplicationController
   def index
-    categories = %w(general puppetfacts vmware upgrades)
+    categories = %w(general puppetfacts vmware)
     records = Server.includes(:details).where(details: { category: categories })
     @servers = convert_yaml(records)
   end
