d71206eb54383e26629671fc81f546d11d2b5b89
Jon Robertson <jonrober@stanford.edu>
2017-04-06 11:04:24 -0700

    Skip hosts that only have update info and nothing else

--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -1,2 +1,17 @@
 module SummaryHelper
+
+  # Decide if a host is expired.  We do this by checking to see if the server
+  # has less than some required amount of data.  This is done because the
+  # 'upgrades' detail will cache data for several weeks, leading to servers
+  # that have otherwise been retired to appear.  So if a host has only upgrades
+  # data, consider it expired.
+  def expired_host(server)
+    server.keys.each do |detail|
+      next if detail == 'upgrades'
+      next unless server[detail].is_a?(Hash)
+      return false if server[detail].keys.count > 0
+    end
+
+    true
+  end
 end
