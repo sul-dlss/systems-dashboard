4332362ac004ae0754e9562aed05af37869dd309
Jon Robertson <jonrober@stanford.edu>
2019-11-21 12:11:32 -0800

    Update capfile for new version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.11.0'
+lock '3.11.2'
 
 set :application, 'systems-dashboard'
 set :repo_url, 'http://github.com/sul-dlss/systems-dashboard.git'
