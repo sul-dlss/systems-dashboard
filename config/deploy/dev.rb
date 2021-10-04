1aff168a7f3c3f0d1d654ad6d983336e913c37df
Jon Robertson <jonrober@stanford.edu>
2021-10-04 10:49:24 -0700

    Update gem requirements

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,8 +1,11 @@
 set :deploy_to, '/home/reporting/systems-dashboard'
-server 'sulreports-dev.stanford.edu', user: 'reporting',
-                                              roles: %w(web db app)
+
+server 'sulreports-dev.stanford.edu',
+  user: 'reporting',
+  roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :branch, 'main'
 set :rails_env, 'development'
+set :log_level, :debug
